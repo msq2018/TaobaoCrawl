0f469ccfe24efa79ff7ec0e7f5cdac5a7e916e48
unknown <834780700@qq.com>
2018-02-24 00:44:38 +0800

    update

--- app/Http/Controllers/ExampleController.php
+++ app/Http/Controllers/ExampleController.php
@@ -5,19 +5,31 @@ namespace App\Http\Controllers;
 
 
 
+
+use Goutte\Client;
+use Shenjian\ShenjianClient;
+
 class ExampleController extends Controller
 {
 
     public function index()
     {
-
-        $crawler = \Goutte::request('GET', 'http://baidu.com');
-        $crawler->filter('.title')->each(function ($node) {
-        dump($node->text());
-    });
-
-    //return view('welcome');
-        
+        $user_key = "9909134dbe-M2IxNjA5MW";
+        $user_secret = "Y5OTA5MTM0ZGJlMz-326f4e354b3b160";
+        $params['app_name'] = "test";
+        $params['app_info'] = "test crawler";
+        $params['code'] = base64_encode("alert(1)");
+        try{
+            $shenjian_client = new ShenjianClient($user_key, $user_secret);
+            $crawler = $shenjian_client->createCrawler($params);
+        }catch (ShenjianException $e){
+            printf($e->getMessage() . "\n");
+            return;
+        }
+        print("Crawler AppId: " . $crawler->getAppId() . "\n");
+        print("Crawler Name: " . $crawler->getName() . "\n");
+        print("Crawler Status: " . $crawler->getStatus() . "\n");
+        print("Crawler TimeCreate: " . $crawler->getTimeCreate());
     }
 
    
